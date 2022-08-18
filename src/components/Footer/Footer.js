d33f5a49bd4800ba7ef95a19123cabbbcfc65d71
MaximeKerkheide <maxime.kerkheide@hotmail.fr>
2022-08-18 16:45:10 +0200

    add and custom the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,25 @@
 import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
+import Button from '@mui/material/Button';
+
+import './Footer.scss';
+
+const pages = [
+  {name:"Mentions légales", path: "/mentions-legales"},
+  {name:"Politique des cookies", path: "/politique-cookies"},
+  {name:"A propos", path:"/a-propos"},
+  {name:"FAQ", path:"/faq"},
+];
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary">
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        Livres O'Trésor
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -20,31 +29,15 @@ function Copyright() {
 
 export default function StickyFooter() {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '100vh',
-      }}
-    >
-      <CssBaseline />
-      <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
-        <Typography variant="h2" component="h1" gutterBottom>
-          Sticky footer
-        </Typography>
-        <Typography variant="h5" component="h2" gutterBottom>
-          {'Pin a footer to the bottom of the viewport.'}
-          {'The footer will move as the main element of the page grows.'}
-        </Typography>
-        <Typography variant="body1">Sticky footer placeholder.</Typography>
-      </Container>
       <Box
+        className="footer"
         component="footer"
         sx={{
           py: 3,
           px: 2,
           mt: 'auto',
-          backgroundColor: (theme) =>
+          backgroundColor: 
+          (theme) =>
             theme.palette.mode === 'light'
               ? theme.palette.grey[200]
               : theme.palette.grey[800],
@@ -52,11 +45,20 @@ export default function StickyFooter() {
       >
         <Container maxWidth="sm">
           <Typography variant="body1">
-            My sticky footer can be found here.
+           
           </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page.name}
+                sx={{ my: 2, color: 'black', display: 'block' }}
+              >
+                {page.name}
+              </Button>
+            ))}
+          </Box>
           <Copyright />
         </Container>
       </Box>
-    </Box>
   );
 }
