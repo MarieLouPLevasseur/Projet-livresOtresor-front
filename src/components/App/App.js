51d4f087b8e817b225a51ff55ec604a28f1da1c5
MaximeKerkheide <maxime.kerkheide@hotmail.fr>
2022-08-18 15:38:03 +0200

    create components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,6 +17,7 @@ import Search from '../Search/Search'
 import MyBooks from '../MyBooks/MyBooks'
 import Rewards from '../Rewards/Rewards'
 import Book from '../Book/Book'
+import Footer from '../Footer/Footer'
 
 import './App.scss';
 
@@ -41,6 +42,7 @@ function App() {
         <Route path="/recompenses" element={<Rewards />} />
         <Route path="/recherche/voir-livre" element={<Book />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
