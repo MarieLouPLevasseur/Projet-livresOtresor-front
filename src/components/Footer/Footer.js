039bf6d444a157c6f9877e30e9f855d347b9b518
MaximeKerkheide <maxime.kerkheide@hotmail.fr>
2022-08-23 00:08:16 +0200

    register page done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -29,7 +29,7 @@ function Copyright() {
 
 export default function Footer() {
   const { pathname } = useLocation();
-  if (pathname === "/connexion-parent" || pathname === "/connexion-enfant") return null;
+  if (pathname === "/connexion-parent" || pathname === "/connexion-enfant" || pathname === "/inscription") return null;
   return (
       <Box
         className="footer"
