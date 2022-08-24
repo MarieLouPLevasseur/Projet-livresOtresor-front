5557a5f1ad0764072bb3b621336361ea74352b42
cedric-cochard <c.jc@live.fr>
2022-08-24 10:42:54 +0200

    faq done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import HomeCarousel from './HomeCarousel/HomeCarousel'
+import Faq from './Faq/Faq'
 
 import './Home.scss'
 
+
 function Home() {
   return (
-    <>
+    <div>
       <HomeCarousel />
-    </>
+      <div> 
+        <h1>gvzhvzadhzevbhvheadhzjzhebvjzhbvhbvfehvbfhzbvfzhjvbfjhvfehvzfgzvfgh</h1>
+      </div>
+      <Faq />
+    </div>
   )
 }
 
