3898881bf6ccac6a18838150033edae047b2a22a
cedric-cochard <c.jc@live.fr>
2022-09-08 11:31:47 +0200

    patch home + button show a book

--- src/components/Book/BookButton/BookButton.js
+++ src/components/Book/BookButton/BookButton.js
@@ -7,12 +7,12 @@ function BookButton() {
   return (
     <div>
        <Box sx={{ flexGrow: 1, display:'flex', flexDirection: 'column', width:'100%'}}>
-          <Button className="button"
+          <Button className="buttonBookAdd"
             sx={{ my: 2, color: 'white', background:'#4462A5' , fontFamily:'Montserrat', width:'100%'}}
           >
                 Ajouter à ma liste d'envie
           </Button>
-          <Button className="button"
+          <Button className="buttonBookAdd"
             sx={{ my: 2, color: 'white',fontFamily:'Montserrat', background:'#4462A5', width:'100%' }}
           >
                 Ajouter à mes livres lus
