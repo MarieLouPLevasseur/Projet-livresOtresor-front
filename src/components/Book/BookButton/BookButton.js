0475ac88af9b8d1c070bb0bf28ef219c62f9393c
MarieLouPLevasseur <sunnymary007@hotmail.com>
2022-09-23 13:19:15 +0200

    set alert when new book add successfully + redirection

--- src/components/Book/BookButton/BookButton.js
+++ src/components/Book/BookButton/BookButton.js
@@ -1,6 +1,10 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { Box, Button } from '@mui/material'
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
 import axios from 'axios';
 
@@ -23,10 +27,28 @@ function BookButton(Book) {
   // Error states
   const [alertErrorSubmit, setAlertErrorSubmit] = useState(false);
   const [alertErrorLogin, setAlertErrorLogin] = useState(false);
+  const [alertSuccesSubmit, setAlertSuccesSubmit] = useState(false);
+
 
   // Api EndPoint
   const apiEndpointCreateBook = `/api/v1/kids/${kidId}/books`
 
+  // Redirect when connected
+  const navigate = useNavigate();
+
+
+
+  // Redirect when success
+  //  TODO pour l'instant redirection vers "mes livres" car il faudrait récupérer l'isbn du nouveau livres avec la route "last-read" qui contiendra l'isbn du dernier livre enregistré
+      //* TODO vérifier qu'en back il ne s'agit pas uniquement du dernier livre lu (sinon il faudra une nouvelle route ou un filtre sur "tous mes livres")
+      //* ou plus simplement mettre en homepage le dernier livre enregistré (et non pas lu) et mettre un clique pour que l'enfant puisse le consulter
+  useEffect(() => {
+    if (alertSuccesSubmit) {
+      setTimeout(() => {
+        navigate("/profil/enfant")
+      }, 2000);
+    }
+  });
 
  //---- call API for Submit form  -------
 
@@ -40,7 +62,11 @@ function BookButton(Book) {
   .catch(function (error) {
     console.log(error);
     setAlertErrorLogin(true)
-  });
+  })
+  .then(function (response) {
+    console.log(response);
+    setAlertSuccesSubmit(true);
+  })
 }
   
 
@@ -57,7 +83,7 @@ function BookButton(Book) {
         "isbn" :       Book.Book.isbn13,
         "cover":       Book.Book.image,
         "publisher":   Book.Book.publisher,
-        "description": Book.Book.synopsis,
+        "description": Book.Book.synopsis !=="" ? "Il n'y a pas de description pour ce livre" : Book.Book.synopsis,
         "title":       Book.Book.title,
         "authors":     [{"name": Book.Book.authors[0]}],
         
@@ -77,6 +103,35 @@ function BookButton(Book) {
   return (
     
     <div>
+
+<Snackbar
+        open={alertSuccesSubmit}
+        autoHideDuration={6000}
+        onClose={() => setAlertSuccesSubmit(false)}
+      >
+        <MuiAlert
+          elevation={6}
+          variant="filled"
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          Le livre a été enregistré avec succès !
+        </MuiAlert>
+      </Snackbar>
+      <Snackbar
+        open={alertErrorSubmit}
+        autoHideDuration={6000}
+        onClose={() => setAlertErrorSubmit(false)}
+      >
+        <MuiAlert
+          elevation={6}
+          variant="filled"
+          severity="error"
+          sx={{ width: "100%" }}
+        >
+          Il y a eut un soucis lors de l'enregistrement du livre. Nous sommes désolés.
+        </MuiAlert>
+      </Snackbar>
 
           {console.log(Book, "info book dans bookbutton")}
 
