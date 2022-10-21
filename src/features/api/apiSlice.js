e4b7af40e9add465038198fc8b795d81397a83c4
MarieLouPLevasseur <sunnymary007@hotmail.com>
2022-10-21 18:42:47 +0200

    change API login

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,8 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  apiUrl: 'http://marie-lou-prince-levasseur.vpnuser.lan:8000',
+  // apiUrl: 'http://marie-lou-prince-levasseur.vpnuser.lan:8000',
+  apiUrl: 'http://localhost:8000',
 }
+
 
 export const apiSlice = createSlice({
   name: 'api',
