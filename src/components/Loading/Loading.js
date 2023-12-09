8fa8929b6aca4807edcf68063548d3d5a6c2cb67
MarieLouPLevasseur <sunnymary007@hotmail.com>
2023-12-09 16:23:57 +0100

    Changement loading et stockage images Avatars

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,8 +1,23 @@
 import './Loading.scss';
 
 function Loading() {
+  // return (
+  //   <div className="chaotic-orbit" />
+  // );
+
   return (
-    <div className="chaotic-orbit" />
+    <div class="bookshelf_wrapper">
+    <ul class="books_list">
+      <li class="book_item first"></li>
+      <li class="book_item second"></li>
+      <li class="book_item third"></li>
+      <li class="book_item fourth"></li>
+      <li class="book_item fifth"></li>
+      <li class="book_item sixth"></li>
+    </ul>
+    <div class="shelf"></div>
+    <div class="loadingBook-text"> lecture en cours ...</div>
+  </div>
   );
 }
 
